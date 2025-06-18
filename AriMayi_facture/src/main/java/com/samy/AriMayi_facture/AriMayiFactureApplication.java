091229a9de56c8ad3bdf7ba264bc19ad941b7744
samy-belazzoug <samy.belazzoug@laplateforme.io>
2025-06-18 18:38:11 +0200

    PROJECT INITIALIZED (JavaSpringBoot, Maven, MVC Files

---
+++ AriMayi_facture/src/main/java/com/samy/AriMayi_facture/AriMayiFactureApplication.java
@@ -0,0 +1,13 @@
+package com.samy.AriMayi_facture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AriMayiFactureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AriMayiFactureApplication.class, args);
+	}
+
+}
---
+++ AriMayi_facture/src/test/java/com/samy/AriMayi_facture/AriMayiFactureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samy.AriMayi_facture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AriMayiFactureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
